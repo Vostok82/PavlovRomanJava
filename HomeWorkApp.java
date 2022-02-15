f996843aa632d9cc2bd013d7f589205a1ebb5bde
Roman Pavlov <vostok1982@bk.ru>
2022-02-15 21:03:39 +0200

    Lesson 01

---
+++ HomeWorkApp.java
@@ -0,0 +1,47 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+    printThreeWords();
+    checkSumSign();
+    printColor();
+    compareNumbers();
+    }
+        public static void printThreeWords() {
+            System.out.println("Orange\nBanana\nApple");
+        }
+        public static void checkSumSign() {
+            int a = 10;
+            int b = 20;
+            if ((a + b) >= 0) {
+                System.out.println("Сумма положительная");
+            } else {
+                System.out.println("Сумма отрицательная");
+            }
+        }
+        public static void printColor() {
+            int value = 100;
+            if (value <= 0) {
+                System.out.println("Красный");
+            }
+            else if (value > 0 && value <= 100) {
+                System.out.println("Жёлтый");
+            }
+            else {
+                System.out.println("Зелёный");
+            }
+        }
+        public static void compareNumbers(){
+            int a = 7;
+            int b = 12;
+            if (a >= b) {
+                System.out.println("a >= b");
+            } else {
+                System.out.println("a < b");
+            }
+        }
+}
+
+
+
+
+
+
